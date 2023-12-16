410db18e507c14efd0821d3d2ad7e8e93fe1c147
big-rodrigo <somentechorume@protonmail.com>
2023-12-16 12:18:24 -0300

    refactor: Change logic of getting quizz result so it doesn't get O(n^2) complexity

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,78 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 import quizz_questions from "../../../assets/data/quizz_questions.json"
 
+type Question = {
+  id: number,
+  question: string,
+  options: {
+    id: number,
+    name: string,
+    alias: string
+  }[]
+};
+
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-
 export class QuizzComponent implements OnInit {
 
-  title:string = ""
+  title: string = "";
 
-  questions:any
-  questionSelected:any
+  questions: Question[] = [];
 
-  answers:string[] = []
-  answerSelected:string =""
+  answers: { [key: string]: number } = {};
+  answerSelected: string = "";
 
-  questionIndex:number =0
-  questionMaxIndex:number=0
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
 
-  finished:boolean = false
+  finished: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-    if(quizz_questions){
-      this.finished = false
-      this.title = quizz_questions.title
 
-      this.questions = quizz_questions.questions
-      this.questionSelected = this.questions[this.questionIndex]
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
 
-      this.questionIndex = 0
-      this.questionMaxIndex = this.questions.length
+      this.questions = quizz_questions.questions;
 
-      console.log(this.questionIndex)
-      console.log(this.questionMaxIndex)
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length - 1;
+
+      for (const option in quizz_questions.results) {
+        this.answers[option] = 0;
+      }
     }
 
   }
 
-  playerChoose(value:string){
-    this.answers.push(value)
-    this.nextStep()
-
+  playerChoose(value: string): void {
+    this.answers[value] += 1;
+    this.nextStep();
+    console.log(this.answers);
   }
 
-  async nextStep(){
-    this.questionIndex+=1
-
-    if(this.questionMaxIndex > this.questionIndex){
-        this.questionSelected = this.questions[this.questionIndex]
-    }else{
-      const finalAnswer:string = await this.checkResult(this.answers)
+  nextStep(): void {
+    
+    if (this.questionMaxIndex >= this.questionIndex + 1) {
+      this.questionIndex += 1;
+    } else {
+      const finalAnswer: string = this.checkResult();
       this.finished = true
-      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results ]
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
     }
   }
 
-  async checkResult(anwsers:string[]){
+  checkResult(): string {
 
-    const result = anwsers.reduce((previous, current, i, arr)=>{
-        if(
-          arr.filter(item => item === previous).length >
-          arr.filter(item => item === current).length
-        ){
-          return previous
-        }else{
-          return current
-        }
-    })
+    let maxValueKey = '';
+    for (const resultKey in this.answers) {
+      if (this.answers[resultKey] > (this.answers[maxValueKey] || 0)) {
+        maxValueKey = resultKey;
+      }
+    }
 
-    return result
+    return maxValueKey;
   }
 
 }
